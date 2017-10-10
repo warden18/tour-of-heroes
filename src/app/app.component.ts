67d54488a2d6e06c93fbde82337f265522ca6643
Roman Daneliia <akariqt@gmail.com>
2017-10-10 21:56:38 +0300

    Create hero-details component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HEROES } from './mock-heroes';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
+  providers: [ HeroService ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'appx';
+  hero: Hero = {
+  	id: 1,
+  	name: 'Flash'
+  };
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor (private heroService: HeroService) {
+    
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes() {
+    this.heroService.getHeroes().then((heroes) => this.heroes = heroes); 
+  }
+
+  onSelect(hero: Hero): void {
+	  this.selectedHero = hero;
+  }
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+	{ id: 11, name: 'Batman' },
+	{ id: 12, name: 'Wonder Woman' },
+	{ id: 13, name: 'Aquaman' },
+	{ id: 14, name: 'Superman' }
+];
