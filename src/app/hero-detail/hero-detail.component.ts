cdbd4e644509574ed6cfd5aafde360205960fd0f
warden18 <akariqt@gmail.com>
2017-10-13 16:02:54 +0300

    Update hero by id

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -24,5 +24,10 @@ export class HeroDetailComponent implements OnInit {
 
 	goBack(): void {
 		this.location.back();
-	}	
+	}
+
+	save (): void {
+		this.heroService.update(this.hero)
+			.then(() => this.goBack());
+	}
 }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { Http } from '@angular/http';
+import { Headers, Http } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class HeroService {
+	private headers = new Headers({'Content-Type': 'application/json'});
+
 	constructor(private http: Http){}
 
 	getHeroes(): Promise<Hero[]> {
@@ -22,5 +24,12 @@ export class HeroService {
 		return this.http.get(`api/heroes/${id}`)
 			.toPromise()
 			.then(response => response.json() as Hero)
+	}
+
+	update (hero: Hero): Promise<Hero> {
+		return this.http.put(`api/heroes/${hero._id}`, JSON.stringify(hero), { headers: this.headers })
+			.toPromise()
+			.then(() => hero)
+			.catch(this.handleError);
 	}
 }
